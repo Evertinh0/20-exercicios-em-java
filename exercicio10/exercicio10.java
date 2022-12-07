410cfd9696c472e8f8cd5cc72ac4249a83a70fee
Everton yan <111475791+Evertinh0@users.noreply.github.com>
2022-12-06 22:21:59 -0300

    Add files via upload

---
+++ exercicio10/exercicio10.java
@@ -0,0 +1,13 @@
+package exercicio10;
+
+public class exercicio10 {
+    public static void main(String[]args) {
+
+        int cont = 0;
+
+       for (cont = 100; cont >= 1; cont--) {
+            System.out.println(cont);
+       }
+
+    }
+}
