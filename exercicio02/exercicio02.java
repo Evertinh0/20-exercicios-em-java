e503be2e2c4418fe0be9c2c8e5a51ebedb57c146
Everton yan <111475791+Evertinh0@users.noreply.github.com>
2022-12-06 22:00:15 -0300

    Add files via upload

---
+++ exercicio02/exercicio02.java
@@ -0,0 +1,24 @@
+package exercicio02;
+
+public class exercicio02 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 9;
+
+        System.out.println("A: "+a);
+        System.out.println("B: "+b);
+
+        if (a == b) {
+            System.out.println("Os numeros de A e B são iguais.");
+        } else {
+            System.out.println("Os numeros de A e B não são iguais.");
+        }
+        if (a > b) {
+            System.out.println("O numero da letra A é maior que o numero da letra B.");
+        } else {
+            System.out.println("O numero da letra B é maior que o numero da letra A.");
+        }
+        
+    }
+}
